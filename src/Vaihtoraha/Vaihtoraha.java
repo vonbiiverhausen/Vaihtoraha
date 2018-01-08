735fa38e3494d58d0944fd95458ed449a1606f6c
Kari Piukka <kari.piukka@gmail.com>
2018-01-08 13:13:14 +0200

    Vaihtoraha

--- src/Vaihtoraha/Vaihtoraha.java
+++ src/Vaihtoraha/Vaihtoraha.java
@@ -20,7 +20,11 @@ public class Vaihtoraha {
                 syote = syote.replace(',', '.');
             }
             summa = Double.parseDouble(syote);
+            if (summa < 0) {
+                summa *= -1;
+            }
             for (int i = rahanArvot.length - 1; i >= 0; i--) {
+                System.out.println(rahanArvot[i]);
                 if ((int) (summa / rahanArvot[i]) > 0) { // jos seteliä / kolikkoa ei tarvita, niin sitä ei tulosteta
                     if (rahanArvot[i] < 1) {
                         tulos += String.format("%d kpl %.2f euron seteliä/kolikkoa\n", (int) (summa / rahanArvot[i]), rahanArvot[i]);
